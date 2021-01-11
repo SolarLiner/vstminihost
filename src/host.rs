768d9924c9375a51b533f75a99e728eaea8ca1de
Nathan Graule <solarliner@gmail.com>
2021-01-11 22:28:16 +0100

    wip: app shell with plugin loading

---
+++ src/host.rs
@@ -0,0 +1,49 @@
+use std::{
+    path::Path,
+    sync::{Arc, Mutex},
+};
+
+use vst::{
+    host::{Host, PluginInstance, PluginLoadError, PluginLoader},
+    plugin::Plugin,
+};
+
+#[derive(Default)]
+struct VstHostImpl {
+    plugin: Option<PluginInstance>,
+}
+
+impl VstHostImpl {
+    fn load(&mut self, mut loader: PluginLoader<Self>) -> Result<(), PluginLoadError> {
+        let mut instance = loader.instance()?;
+        instance.init();
+        self.plugin.replace(instance);
+        Ok(())
+    }
+
+    fn get_instance(&self) -> Option<&PluginInstance> {
+        self.plugin.as_ref()
+    }
+}
+
+#[derive(Default)]
+pub struct VstHost(Arc<Mutex<VstHostImpl>>);
+
+impl Host for VstHostImpl {}
+
+impl VstHost {
+    pub fn new() -> Self {
+        Self(Default::default())
+    }
+
+    pub fn load<P: AsRef<Path>>(&self, path: P) -> Result<(), PluginLoadError> {
+        let path = path.as_ref();
+
+        let loader = PluginLoader::load(path, self.0.clone())?;
+        self.0.lock().unwrap().load(loader)
+    }
+
+    pub fn get_ref_map<T, F: FnOnce(&PluginInstance) -> T>(&self, func: F) -> Option<T> {
+        self.0.lock().unwrap().get_instance().map(func)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
+use host::VstHost;
+use tuix::*;
+
+use nfd::Response;
+
+mod host;
+
+#[derive(Clone, Debug, PartialEq)]
+enum HostWidgetEvent {
+    OpenFile,
+}
+
+#[derive(Default)]
+pub struct HostWidget {
+    host: VstHost,
+}
+
+impl HostWidget {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl BuildHandler for HostWidget {
+    type Ret = Entity;
+    fn on_build(&mut self, state: &mut State, entity: Entity) -> Self::Ret {
+        state.insert_stylesheet("src/theme.css");
+
+        let open_file_button = Button::with_label("Open File")
+            .on_press(Event::new(HostWidgetEvent::OpenFile).target(entity))
+            .build(state, entity, |builder| {
+                builder.class("open_file_dialogue_button")
+            });
+
+        entity
+    }
+}
+
+impl EventHandler for HostWidget {
+    fn on_event(&mut self, state: &mut State, entity: Entity, event: &mut Event) -> bool {
+        if let Some(host_widget_event) = event.message.downcast::<HostWidgetEvent>() {
+            match host_widget_event {
+                HostWidgetEvent::OpenFile => {
+                    let result = nfd::open_file_dialog(None, None).unwrap_or_else(|e| {
+                        panic!(e);
+                    });
+
+                    let path = match result {
+                        Response::Okay(file_path) => file_path,
+                        Response::OkayMultiple(mut files) => {
+                            files.drain(..).take(1).next().unwrap()
+                        }
+                        _ => return false,
+                    };
+                    match self.host.load(path) {
+                        Ok(()) => {}
+                        Err(err) => eprintln!("Plugin load error: {}", err),
+                    }
+                }
+            }
+        }
+
+        false
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::new(|win_desc, state, window| {
+        let host_widget =
+            HostWidget::new().build(state, window, |builder| builder.set_flex_grow(1.0));
+
+        win_desc
+    });
+
+    app.run();
 }
